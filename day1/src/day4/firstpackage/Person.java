cee00a2e7a6a63a6bc8944d256562f1513ce1d56
dhine <dhine@Dk>
2025-09-30 09:33:42 +0530

    commit

---
+++ day1/src/day4/firstpackage/Person.java
@@ -0,0 +1,25 @@
+package day4.firstpackage;
+
+public class Person {
+	public class Person {
+		private String personName;
+		private int personAge;
+		private String personCity;
+		
+		public String getPersonName() {
+			return personName;
+		}
+
+		public void setPersonName(String personName) {
+			this.personName = personName;
+		}
+
+		public int getPersonAge() {
+			return personAge;
+		}
+
+		public void setPersonAge(int personAge) {
+			this.personAge = personAge;
+		}
+
+}
