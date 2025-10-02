3dd078342cf698a6b8b7a0417cddaea09567624e
dhine <dhine@Dk>
2025-10-02 21:53:51 +0530

    commit

--- day1/src/day5/singlelevelinheritance/SLInheritanceDemo.java
+++ day1/src/day5/singlelevelinheritance/SLInheritanceDemo.java
@@ -1,63 +1,14 @@
 package day5.singlelevelinheritance;
 
 public class SLInheritanceDemo {
-	public class Citizen {
-		private String name;
-		private String aadharNo;
-		private String address;
-		private long phno;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Citizen c = new Citizen("Partha", "258741962525", "Chennai", 9159186845L);
+		System.out.println(c);
 		
-		
-
-		public Citizen() {
-			System.out.println("Citizen object created");
-		}
-
-		public Citizen(String name, String aadharNo, String address, long phno) {
-			
-			this.name = name;
-			this.aadharNo = aadharNo;
-			this.address = address;
-			this.phno = phno;
-		}
-
-		public String getName() {
-			return name;
-		}
-
-		public void setName(String name) {
-			this.name = name;
-		}
-
-		public String getAadharNo() {
-			return aadharNo;
-		}
-
-		public void setAadharNo(String aadharNo) {
-			this.aadharNo = aadharNo;
-		}
-
-		public String getAddress() {
-			return address;
-		}
-
-		public void setAddress(String address) {
-			this.address = address;
-		}
-
-		public long getPhno() {
-			return phno;
-		}
-
-		public void setPhno(long phno) {
-			this.phno = phno;
-		}
-
-		@Override
-		// toString() - string representation of an object
-		public String toString() {
-			return "Citizen [name=" + name + ", aadharNo=" + aadharNo + ", address=" + address + ", phno=" + phno + "]";
-		}
-
+		Student student = new Student("Gayatri", "8080808080", "Bangalore", 9888878787L, 10, "City College");
+		System.out.println(student);
 	}
+
 }
