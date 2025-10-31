aad0d440542e2227df9b87c2c1a5dc8042b57000
dhine <dhine@Dk>
2025-10-31 15:45:58 +0530

    commit

--- day1/src/day7/overloading/v1/overriding/covarient/CovariantOverriding.java
+++ day1/src/day7/overloading/v1/overriding/covarient/CovariantOverriding.java
@@ -15,9 +15,8 @@ public class CovariantOverriding {
 			Red s = new Red();
 			return s;
 		}
-	}
 
-	public class CovariantOverriding {
+
 		public static void main(String[] args) {
 			Color c1 = new Color();
 			Color c2 = c1.getColor();
@@ -27,4 +26,5 @@ public class CovariantOverriding {
 			Red r2 = r1.getColor();
 			System.out.println(r2);
 		}
+		
 	}
