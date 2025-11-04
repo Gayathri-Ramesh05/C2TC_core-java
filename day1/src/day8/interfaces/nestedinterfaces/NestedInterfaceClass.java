b29c9efc55fde122bd53c3f8535523c6e0729288
dhine <dhine@Dk>
2025-11-04 11:26:31 +0530

    commit

---
+++ day1/src/day8/interfaces/nestedinterfaces/NestedInterfaceClass.java
@@ -0,0 +1,11 @@
+package day8.interfaces.nestedinterfaces;
+
+public class NestedInterfaceClass {
+	public static char[] id;
+
+	public void print() {
+		// TODO Auto-generated method stub
+		 System.out.println("Print method of nested interface");
+	}
+
+}
