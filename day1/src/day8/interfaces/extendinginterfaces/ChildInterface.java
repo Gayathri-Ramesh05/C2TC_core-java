1e45aa18d9fd2ad33c3c1673fb738d06819a6c65
dhine <dhine@Dk>
2025-11-04 10:48:27 +0530

    commit

---
+++ day1/src/day8/interfaces/extendinginterfaces/ChildInterface.java
@@ -0,0 +1,6 @@
+package day8.interfaces.extendinginterfaces;
+
+public class ChildInterface {
+	void show() {
+	}
+}
---
+++ day1/src/day8/interfaces/extendinginterfaces/ExtendingInterfaceDemo.java
@@ -0,0 +1,12 @@
+package day8.interfaces.extendinginterfaces;
+
+public class ExtendingInterfaceDemo {
+	public void print() { 
+		System.out.println("print method");
+	}
+
+	// Override InterfaceOne method
+	public void show() {
+		System.out.println("show Method");
+	}
+}
