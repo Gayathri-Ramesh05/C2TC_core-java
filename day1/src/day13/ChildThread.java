15c21c06a1b1c234ad92638d1e8aba0ed8b90a2c
dhine <dhine@Dk>
2025-11-05 10:56:02 +0530

    commit

---
+++ day1/src/day13/ChildThread.java
@@ -0,0 +1,33 @@
+package day13;
+
+public class ChildThread {
+	private int n;
+	private String msg;
+
+	// Constructor
+	public ChildThread(int n, String msg) {
+		this.n = n;
+		this.msg = msg;
+	}
+
+	public void run() {
+		for (int i = 1; i <= n; i++) {
+			try {
+				Thread.sleep(300);
+			} catch (InterruptedException e) {
+				System.err.println("Thread interrupted: " + e.getMessage());
+			}
+			System.out.println(msg + i + " " + Thread.currentThread().getName());
+		}
+	}
+
+	public void start() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void join() {
+		// TODO Auto-generated method stub
+		
+	}
+}
