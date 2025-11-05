5ea3b2bfa7de1d51cfd8bfe1971f5c35018bade0
dhine <dhine@Dk>
2025-11-05 10:49:40 +0530

    commit

---
+++ day1/src/day12/InvalidMarksException.java
@@ -0,0 +1,17 @@
+package day12;
+
+public class InvalidMarksException {
+
+	public InvalidMarksException() {
+		super();
+	}
+
+	public InvalidMarksException(String message) {
+		super();
+	}
+
+	public char[] getMessage() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
---
+++ day1/src/day12/Service.java
@@ -0,0 +1,16 @@
+package day12;
+
+public class Service {
+
+	public static float calculatePercentage(int[] intArr) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public static boolean validateMarks(int[] intArr) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+
+}
